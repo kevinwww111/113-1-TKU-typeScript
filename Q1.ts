e4fdc61e6dc7c1dc2d50233a51b716872e0fd425
kevinwww111 <132751316+kevinwww111@users.noreply.github.com>
2024-10-29 19:20:51 +0800

    Add files via upload

---
+++ Q1.ts
@@ -0,0 +1,13 @@
+const age: number = 25;
+
+// 嘗試更改常數的值
+age = 30; 
+
+// 聲明一個可以改變的變數
+let name: string = "Alice";
+
+// 更改變數的值
+name = "Bob"; 
+
+console.log(`Age: ${age}`);
+console.log(`Name: ${name}`);
---
+++ Q2.ts
@@ -0,0 +1,8 @@
+for (let i = 1; i <= 10; i++) {
+    // 使用 if 判斷奇數或偶數
+    if (i % 2 === 0) {
+      console.log(`${i} is even`);
+    } else {
+      console.log(`${i} is odd`);
+    }
+  }
